7588377419221aa681c592b9997ce33599fadce4
Justin Geeslin <justin@geesl.in>
2017-04-20 14:27:34 -0500

    Sensible namespace + exports

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-window.sensible.classes.Accordion = require('./js/sensibleAccordion.js')
+window.sensible = typeof sensible !== "undefined" ? sensible : {};
+sensible.classes = typeof sensible.classes !== "undefined" ? sensible.classes : {};
+
+window.sensible.classes.Accordion = require('./js/sensibleAccordion.js');
+
+module.exports = window.sensible.classes.Accordion;
