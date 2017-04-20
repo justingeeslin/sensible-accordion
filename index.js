b81867c0c1c125c717e5232add0d448cadf4d465
Justin Geeslin <justin@geesl.in>
2017-04-20 14:07:51 -0500

    using the sensible namespace

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-// Create a list of Conversation Segment pairs. [ {key, url, title, timeStart, timeEnd, [{interest, score}]}]
-const Segment = require('./js/Segment.js')
-var myFirstSegment = new Segment();
+window.sensible.classes.Accordion = require('./js/sensibleAccordion.js')
